f04ad35dbb1f80f9fbdb2718ea632d5336d81f2c
Dongjun Park <pdj5096@gmail.com>
2019-06-02 02:36:36 +0900

    Update game.py

--- game.py
+++ game.py
@@ -4,6 +4,11 @@ import requests, hgtk, random
 #이미 있는 단어 알기위해 단어목록 저장
 history = []
 playing = True
+#키 발급은 https://krdict.korean.go.kr/openApi/openApiInfo
+apikey = ''
+
+#좀 치사한 한방단어 방지 목록
+blacklist = ['즘', '틱', '늄', '슘', '퓸', '늬', '뺌', '섯', '숍']
 
 #지정한 두 개의 문자열 사이의 문자열을 리턴하는 함수
 #string list에서 단어, 품사와 같은 요소들을 추출할때 사용됩니다
@@ -25,25 +30,44 @@ def midReturn_all(val, s, e):
         val = []
     return val
 
-def findword(char):
-    url = 'https://krdict.korean.go.kr/api/search?key=[APIKEY]&part=word&pos=1&q=' + char + '*'
+def findword(query):
+    url = 'https://krdict.korean.go.kr/api/search?key=' + apikey + '&part=word&pos=1&q=' + query
     response = requests.get(url)
     ans = []
+    
     #단어 목록을 불러오기
     words = midReturn_all(response.text,'<item>','</item>')
     for w in words:
-        word = midReturn(w, '<word>', '</word>') #단어 불러오기
-        pos = midReturn(w, '<pos>', '</pos>') #품사 불러오기
-        
         #이미 쓴 단어가 아닐때
-        if not (w in history):
-            
+        if not (w in history):           
             #한글자가 아니고 품사가 명사일때
-            if len(word) > 1 and pos == '명사' and not word in history:
-                ans.append(word)
-
+            word = midReturn(w,'<word>','</word>')
+            pos = midReturn(w,'<pos>','</pos>')
+            if len(word) > 1 and pos == '명사' and not word in history and not word[len(word)-1] in blacklist:
+                ans.append(w)
     if len(ans)>0:
         return random.choice(ans)
+    else:
+        return ''
+
+
+def checkexists(query):
+    url = 'https://krdict.korean.go.kr/api/search?key=' + apikey + '&part=word&pos=1&q=' + query
+    response = requests.get(url)
+    ans = ''
+
+    #단어 목록을 불러오기
+    words = midReturn_all(response.text,'<item>','</item>')
+    for w in words:
+        #이미 쓴 단어가 아닐때
+        if not (w in history):           
+            #한글자가 아니고 품사가 명사일때
+            word = midReturn(w,'<word>','</word>')
+            pos = midReturn(w,'<pos>','</pos>')
+            if len(word) > 1 and pos == '명사' and word == query: ans = w
+
+    if len(ans)>0:
+        return ans
     else:
         return ''
 
@@ -71,7 +95,7 @@ while(playing):
     wordOK = False
 
     while(not wordOK):
-        query = input('나>')
+        query = input('나> ')
         wordOK = True
         
         if query == '/그만':
@@ -92,10 +116,10 @@ while(playing):
             
             #첫 글자의 초성 분석하여 두음법칙 적용 -> 규칙에 아직 완벽하게 맞지 않으므로 차후 수정 필요
             if not len(history)==0 and not query[0] == sword and not query=='':
-                if hgtk.letter.decompose(sword)[0] == 'ㄹ' and hgtk.letter.decompose(query[0])[0] == 'ㄴ':
-                    print('두음법칙 적용됨')
-                elif hgtk.letter.decompose(sword)[0] == 'ㄴ' and hgtk.letter.decompose(query[0])[0] == 'ㅇ':
-                    print('두음법칙 적용됨')
+                sdis = hgtk.letter.decompose(sword)
+                qdis = hgtk.letter.decompose(query[0])
+                if sdis[0] == 'ㄹ' and qdis[0] == 'ㄴ': print('두음법칙 적용됨')
+                elif sdis[0] == 'ㄹ' and qdis[0] == 'ㅇ' and qdis[1] in ('ㅑ', 'ㅕ', 'ㅛ', 'ㅠ', 'ㅒ', 'ㅖ'): print('두음법칙 적용됨')
                 else:
                     wordOK = False
                     print(sword + '(으)로 시작하는 단어여야 합니다.')
@@ -108,56 +132,54 @@ while(playing):
                 wordOK = False
                 print('이미 입력한 단어입니다')
 
+            if query[len(query)-1] in blacklist:
+                print('아.. 좀 치사한데요..')
+
             if wordOK:
                 #단어의 유효성을 체크
-                url = 'https://krdict.korean.go.kr/api/search?key=[APIKEY]&part=word&pos=1&q=' + query
-                response = requests.get(url)
-                #단어 목록을 불러오기
-                words = midReturn_all(response.text, '<word>', '</word>') #단어 불러오기
-                #검색 결과에 쿼리가 존재할때
-                actualWords = False
-                for w in words:
-                    if w == query: actualWords = True
-                if not actualWords:
+                ans = checkexists(query)
+                if ans == '':
                     wordOK = False
                     print('유효한 단어를 입력해 주십시오')
+                else:
+                    print('(' + midReturn(ans, '<definition>', '</definition>') + ')\n')
                     
-
     history.append(query)
     
     if playing:       
         start = query[len(query)-1]
 
-        ans = findword(start)
+        ans = findword(start + '*')
 
         if ans=='':
-            #두음법칙 적용하여 재검색
-            sdis = hgtk.letter.decompose(start)
-            if sdis[0] == 'ㄹ':
-               newq = hgtk.letter.compose('ㅇ', sdis[1], sdis[2])
-               print(start, '->', newq)
-               ans = findword(newq)
-
-        if ans=='':
-            #두음법칙 적용하여 재검색
+            #ㄹ -> ㄴ 검색
             sdis = hgtk.letter.decompose(start)
             if sdis[0] == 'ㄹ':
                newq = hgtk.letter.compose('ㄴ', sdis[1], sdis[2])
                print(start, '->', newq)
-               ans = findword(newq)
+               start = newq
+               ans = findword(newq + '*')
+
+        if ans=='':
+            #(ㄹ->)ㄴ -> ㅇ 검색
+            sdis = hgtk.letter.decompose(start)
+            if sdis[0] == 'ㄴ' and sdis[1] in ('ㅣ', 'ㅑ', 'ㅕ', 'ㅛ', 'ㅠ', 'ㅒ', 'ㅖ'):
+                newq = hgtk.letter.compose('ㅇ', sdis[1], sdis[2])
+                print(start, '->', newq)
+                ans = findword(newq + '*')
             
         if ans=='':
             print('당신의 승리!')
             break
         else:
-            history.append(ans)
-            print('컴퓨터>', ans)
+            answord = midReturn(ans, '<word>', '</word>') #단어 불러오기
+            ansdef = midReturn(ans, '<definition>', '</definition>') #품사 불러오기
+            history.append(answord)
+            
+            print('컴퓨터>', answord, '\n('+ansdef+')\n')
             sword = history[len(history)-1][len(history[len(history)-1])-1]
             
             #컴퓨터 승리여부 체크            
             #if findword(sword) == '':
             #    print('tip: \'/다시\'를 입력하여 게임을 다시 시작할 수 있습니다')
-
             
-            
-
